eba0dcf22cb447c1746d0c3b4dd728cf5b24ed5b
Darlish MBAMA <michelmbama01gmail.com>
2022-05-13 09:15:25 +0200

    add fichier MVS_FizzBuzz.h MVS_FizzBuzz.c

---
+++ untitled3/MVS_FizzBuzz.h
@@ -0,0 +1,15 @@
+//
+// Created by miche on 13/05/2022.
+//
+
+#ifndef UNTITLED3_MVS_FIZZBUZZ_H
+#define UNTITLED3_MVS_FIZZBUZZ_H
+
+#ifndef MVS_FIZZBUZZ_H
+#define FIZZ "Fizz"
+#define BUZZ "Buzz"
+#endif // !MVS_FIZZBUZZ_H
+
+
+
+#endif //UNTITLED3_MVS_FIZZBUZZ_H
--- untitled3/cmake-build-debug/CMakeFiles/untitled3.dir/DependInfo.cmake
+++ untitled3/cmake-build-debug/CMakeFiles/untitled3.dir/DependInfo.cmake
@@ -4,6 +4,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_C
+  "C:/Users/miche/CLionProjects/untitled3/ MVS_FizzBuzz.c" "C:/Users/miche/CLionProjects/untitled3/cmake-build-debug/CMakeFiles/untitled3.dir/_MVS_FizzBuzz.c.obj"
   "C:/Users/miche/CLionProjects/untitled3/main.c" "C:/Users/miche/CLionProjects/untitled3/cmake-build-debug/CMakeFiles/untitled3.dir/main.c.obj"
   )
 set(CMAKE_C_COMPILER_ID "GNU")
